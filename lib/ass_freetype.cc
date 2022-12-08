998a774b56ba3e93ff351296b4d3897c0fd337db
wyzdwdz <wyzdwdz@gmail.com>
2022-12-08 13:40:34 +0100

    add copyright info

--- lib/ass_freetype.cc
+++ lib/ass_freetype.cc
@@ -1,3 +1,22 @@
+/*  This file is part of assfonts.
+ *
+ *  assfonts is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation,
+ *  either version 2 of the License,
+ *  or (at your option) any later version.
+ *
+ *  assfonts is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty
+ *  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with assfonts. If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *  written by wyzdwdz (https://github.com/wyzdwdz)
+ */
+
 #include "ass_freetype.h"
 
 #ifdef _WIN32
@@ -19,7 +38,7 @@ void ft_free(FT_Memory memory, void* block) {
 }
 
 void ft_close_stream_by_munmap(FT_Stream stream) {
-  UnmapViewOfFile((LPCVOID)stream->descriptor.pointer);
+  UnmapViewOfFile(static_cast<LPCVOID>(stream->descriptor.pointer));
   stream->descriptor.pointer = NULL;
   stream->size = 0;
   stream->base = NULL;
@@ -37,6 +56,23 @@ void ft_close_stream_by_free(FT_Stream stream) {
 FT_Error NewOpenArgs(const AString& filepathname, FT_StreamRec& stream,
                      FT_Open_Args& args) {
 #ifdef _WIN32
+  /****************************************************************************
+ *
+ * ftsystem.c
+ *
+ *   Windows-specific FreeType low-level system interface (body).
+ *
+ * Copyright (C) 2021-2022 by
+ * David Turner, Robert Wilhelm, and Werner Lemberg.
+ *
+ * This file is part of the FreeType project, and may only be used,
+ * modified, and distributed under the terms of the FreeType project
+ * license, LICENSE.TXT.  By continuing to use, modify, or distribute
+ * this file you indicate that you have read the license and
+ * understand and accept it fully.
+ *
+ */
+
   HANDLE file;
   HANDLE fm;
   LARGE_INTEGER size;
@@ -64,13 +100,15 @@ FT_Error NewOpenArgs(const AString& filepathname, FT_StreamRec& stream,
   }
   stream.size = size.LowPart;
   stream.pos = 0;
-  stream.base = (unsigned char*)MapViewOfFile(fm, FILE_MAP_READ, 0, 0, 0);
+  stream.base =
+      static_cast<unsigned char*>(MapViewOfFile(fm, FILE_MAP_READ, 0, 0, 0));
   CloseHandle(fm);
   if (stream.base != NULL)
     stream.close = ft_close_stream_by_munmap;
   else {
     DWORD total_read_count;
-    stream.base = (unsigned char*)ft_alloc(stream.memory, stream.size);
+    stream.base =
+        static_cast<unsigned char*>(ft_alloc(stream.memory, stream.size));
     if (!stream.base) {
       goto Fail_Open;
     }
@@ -100,6 +138,7 @@ FT_Error NewOpenArgs(const AString& filepathname, FT_StreamRec& stream,
   args.num_params = 0;
   args.params = NULL;
   return FT_Err_Ok;
+
 Fail_Read:
   ft_free(stream.memory, stream.base);
 Fail_Open:
@@ -108,7 +147,9 @@ Fail_Open:
   stream.size = 0;
   stream.pos = 0;
   return FT_Err_Cannot_Open_Stream;
+
 #else
+
   args.flags = FT_OPEN_PATHNAME;
   args.memory_base = NULL;
   args.memory_size = 0;
@@ -118,6 +159,7 @@ Fail_Open:
   args.num_params = 0;
   args.params = NULL;
   return FT_Err_Ok;
+
 #endif
 }
 
