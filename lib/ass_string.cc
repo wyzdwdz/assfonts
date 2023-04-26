d8b3d3d60c4a67673032ef8b5545403f1669ec90
wyzdwdz <wyzdwdz@gmail.com>
2023-04-26 10:59:43 +0000

    refactor ass_string

--- lib/ass_string.cc
+++ lib/ass_string.cc
@@ -42,13 +42,16 @@ namespace ass {
 
 std::string Trim(const std::string& str) {
   std::string res = str;
+
   res.erase(res.begin(),
             std::find_if(res.begin(), res.end(),
                          [](unsigned char ch) { return !std::isspace(ch); }));
+
   res.erase(std::find_if(res.rbegin(), res.rend(),
                          [](unsigned char ch) { return !std::isspace(ch); })
                 .base(),
             res.end());
+
   return res;
 }
 
@@ -80,21 +83,27 @@ AString ToAString(const long i) {
 
 std::istream& SafeGetLine(std::istream& is, std::string& t) {
   t.clear();
+
   std::istream::sentry se(is, true);
   std::streambuf* sb = is.rdbuf();
+
   for (;;) {
     int c = sb->sbumpc();
+
     switch (c) {
       case '\n':
         return is;
+
       case '\r':
         if (sb->sgetc() == '\n')
           sb->sbumpc();
         return is;
+
       case std::streambuf::traits_type::eof():
         if (t.empty())
           is.setstate(std::ios::eofbit);
         return is;
+
       default:
         t += (char)c;
     }
@@ -104,18 +113,24 @@ std::istream& SafeGetLine(std::istream& is, std::string& t) {
 bool IconvConvert(const std::string& in, std::string& out,
                   const std::string& from_code, const std::string& to_code) {
   IconvT cd(to_code.c_str(), from_code.c_str());
+
   if (cd.get() == reinterpret_cast<iconv_t>(-1)) {
     return false;
   }
+
   char* inbuf = const_cast<char*>(in.c_str());
   size_t insize = in.size();
+
   auto outbuf = std::make_unique<char[]>(insize * 4);
   char* outbuf_tmp = outbuf.get();
   size_t outsize = insize * 4;
+
   size_t err = iconv(cd.get(), &inbuf, &insize, &outbuf_tmp, &outsize);
+
   if (err == static_cast<size_t>(-1)) {
     return false;
   }
+
   out = std::string(outbuf.get(), outbuf_tmp - outbuf.get());
   return true;
 }
@@ -123,8 +138,11 @@ bool IconvConvert(const std::string& in, std::string& out,
 std::u32string U8ToU32(const std::string& str_u8) {
   std::u32string str_u32;
   std::string str_u32_narrow;
+
   IconvConvert(str_u8, str_u32_narrow, "UTF-8", "UTF-32BE");
+
   str_u32.resize(str_u32_narrow.size() / 4);
+
   for (size_t i = 0; i < str_u32.size(); ++i) {
     if (endian::native == endian::big) {
       str_u32[i] =
@@ -132,6 +150,7 @@ std::u32string U8ToU32(const std::string& str_u8) {
           (static_cast<unsigned char>(str_u32_narrow[i * 4 + 1]) << 8) |
           (static_cast<unsigned char>(str_u32_narrow[i * 4 + 2]) << 16) |
           (static_cast<unsigned char>(str_u32_narrow[i * 4 + 3]) << 24);
+
     } else {
       str_u32[i] =
           (static_cast<unsigned char>(str_u32_narrow[i * 4 + 3]) << 0) |
@@ -140,13 +159,16 @@ std::u32string U8ToU32(const std::string& str_u8) {
           (static_cast<unsigned char>(str_u32_narrow[i * 4 + 0]) << 24);
     }
   }
+
   return str_u32;
 }
 
 std::string U32ToU8(const std::u32string& str_u32) {
   std::string str_u8;
   std::string str_u32_narrow;
+
   str_u32_narrow.resize(str_u32.size() * 4);
+
   for (size_t i = 0; i < str_u32.size(); ++i) {
     if (endian::native == endian::big) {
       str_u32_narrow[i * 4] =
@@ -157,6 +179,7 @@ std::string U32ToU8(const std::u32string& str_u32) {
           static_cast<char>(static_cast<uint32_t>(str_u32[i]) >> 16);
       str_u32_narrow[i * 4 + 3] =
           static_cast<char>(static_cast<uint32_t>(str_u32[i]) >> 24);
+
     } else {
       str_u32_narrow[i * 4 + 3] =
           static_cast<char>(static_cast<uint32_t>(str_u32[i]) >> 0);
@@ -168,7 +191,9 @@ std::string U32ToU8(const std::u32string& str_u32) {
           static_cast<char>(static_cast<uint32_t>(str_u32[i]) >> 24);
     }
   }
+
   IconvConvert(str_u32_narrow, str_u8, "UTF-32BE", "UTF-8");
+
   return str_u8;
 }
 
@@ -186,11 +211,13 @@ std::wstring U8ToWide(const std::string& str) {
   std::wstring w_str;
   int offset = 0;
   size_t index = 0;
+
   for (wchar_t wc; (offset = std::mbtowc(&wc, str.c_str() + index,
                                          str.length() - index)) > 0;
        index += offset) {
     w_str.push_back(wc);
   }
+
   return w_str;
 #endif
 }
@@ -208,24 +235,29 @@ std::string WideToU8(const std::wstring& w_str) {
   return str;
 #else
   std::string str;
+
   for (const auto& wc : w_str) {
     char c[MB_CUR_MAX];
     int len = std::wctomb(c, wc);
+
     if (len > 0) {
       str.append(std::string(c, len));
     };
   }
+
   return str;
 #endif
 }
 
 int StringToInt(const std::string& str) {
   int res = 0;
+
   try {
     res = std::stoi(str);
   } catch (const std::exception&) {
     res = 0;
   }
+  
   return res;
 }
 
