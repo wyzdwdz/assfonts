25e34aea35a1eb5fb2f79c9ef0dc26ca20a15e85
wyzdwdz <wyzdwdz@gmail.com>
2023-05-22 10:59:44 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -2,20 +2,31 @@
 
 BUILD_TYPE=Release
 
-while getopts "b:" arg
+INSTALL_PREFIX=$(realpath -m install)
+
+while getopts "b:o:" arg
 do
     case $arg in
         b)
             BUILD_TYPE=$OPTARG
+            ;;
+        o)
+            INSTALL_PREFIX=$(realpath -m $OPTARG)
             ;;
         ?)
     exit 1
     ;;
     esac
 done
-        
-conan install . -b missing -s build_type=${BUILD_TYPE} -c tools.system.package_manager:mode=install
+
+if (( $EUID != 0 ))
+then        
+    conan install . -b missing -s build_type=${BUILD_TYPE} -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
+else
+    conan install . -b missing -s build_type=${BUILD_TYPE} -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=False
+fi
+
 cd build
 source conanbuild.sh
-cmake .. --preset conan-${BUILD_TYPE,,}
+cmake .. --preset conan-${BUILD_TYPE,,} -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}
 cmake --build --preset conan-${BUILD_TYPE,,} --target install -j
