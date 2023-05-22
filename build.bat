b01d12fd5a7be0b6b662115e36c93f01e98dd092
wyzdwdz <wyzdwdz@gmail.com>
2023-05-22 17:36:26 +0000

    update build.bat

--- build.bat
+++ build.bat
@@ -10,5 +10,5 @@ for %%i in (a b c d e f g h i j k l m n o p q r s t u v w x y z)  do call set  B
 conan install . -b missing -s build_type=%BUILD_TYPE% -c tools.system.package_manager:mode=install
 cd build
 call conanbuild.bat
-cmake .. --preset conan-default
+cmake .. --preset conan-default -DCMAKE_INSTALL_PREFIX=../install
 cmake --build --preset conan-%BUILD_TYPE_LOW% --target install -j
