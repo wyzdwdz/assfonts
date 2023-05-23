8a3f0b608fb56eacc956acff8f6cfd74d5682365
wyzdwdz <wyzdwdz@gmail.com>
2023-05-23 16:48:32 +0000

    update linux_build.yml

--- build.bat
+++ build.bat
@@ -7,6 +7,7 @@ if "%1" EQU "-b" (set BUILD_TYPE=%~2)
 set BUILD_TYPE_LOW=%BUILD_TYPE%
 for %%i in (a b c d e f g h i j k l m n o p q r s t u v w x y z)  do call set  BUILD_TYPE_LOW=%%BUILD_TYPE_LOW:%%i=%%i%%
 
+conan profile detect -vquiet
 conan install . -b missing -s build_type=%BUILD_TYPE% -c tools.system.package_manager:mode=install
 cd build
 call conanbuild.bat
