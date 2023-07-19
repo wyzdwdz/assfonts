577e728cb0a3ecf62f3586a6c6ea0e2eaa2df276
wyzdwdz <wyzdwdz@gmail.com>
2023-07-19 13:34:42 +0200

    update NEWS

--- src/log_highlighter.h
+++ src/log_highlighter.h
@@ -1,3 +1,22 @@
+/*  This file is part of assfonts.
+ *
+ *  assfonts is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation,
+ *  either version 2 of the License,
+ *  or (at your option) any later version.
+ *
+ *  assfonts is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty
+ *  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with assfonts. If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *  written by wyzdwdz (https://github.com/wyzdwdz)
+ */
+
 #pragma once
 
 #include <QSyntaxHighlighter>
