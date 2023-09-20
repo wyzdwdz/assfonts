00d5e703e170f0bc97625a8c4fb68de41dd68400
wyzdwdz <wyzdwdz@gmail.com>
2023-09-20 21:31:29 +0000

    resolve conflict

--- conanfile.py
+++ conanfile.py
@@ -29,6 +29,7 @@ class Assfonts(ConanFile):
         self.requires("cpp-httplib/0.13.3")
         self.requires("libpng/1.6.40", override=True)
         self.requires("openssl/1.1.1u", override=True)
+        self.requires("zlib/1.2.13", override=True)
 
     def build_requirements(self):
         self.tool_requires("cmake/3.26.3")
