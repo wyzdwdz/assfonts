f10a7bd3790cea17ecae6065f6a743d804c08cb0
wyzdwdz <wyzdwdz@gmail.com>
2023-10-30 13:31:27 +0800

    update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMakeDeps, CMakeToolchain, cmake_layout
 
 class Assfonts(ConanFile):
     name = "assfonts"
-    version = "0.5.5"
+    version = "0.5.6"
 
     license = "GNU General Public License v3.0"
     author = "wyzdwdz"
