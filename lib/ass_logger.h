0ab42e522e5475a1a4b80dcf1a05e690dfb81d8a
wyzdwdz <wyzdwdz@gmail.com>
2024-03-30 22:36:00 +0100

    fix logger bug

--- lib/ass_logger.h
+++ lib/ass_logger.h
@@ -53,6 +53,16 @@ class Logger {
       Log(ASSFONTS_TEXT, fmt, std::forward<T>(args)...);
     }
   }
+  void Text(std::string msg) {
+    if (log_level_ <= ASSFONTS_TEXT) {
+      Log(ASSFONTS_TEXT, msg);
+    }
+  }
+  void Text(std::wstring msg) {
+    if (log_level_ <= ASSFONTS_TEXT) {
+      Log(ASSFONTS_TEXT, msg);
+    }
+  }
 
   template <typename... T>
   void Info(fmt::format_string<T...> fmt, T&&... args) {
@@ -64,6 +74,16 @@ class Logger {
   void Info(fmt::wformat_string<T...> fmt, T&&... args) {
     if (log_level_ <= ASSFONTS_INFO) {
       Log(ASSFONTS_INFO, fmt, std::forward<T>(args)...);
+    }
+  }
+  void Info(std::string msg) {
+    if (log_level_ <= ASSFONTS_INFO) {
+      Log(ASSFONTS_INFO, msg);
+    }
+  }
+  void Info(std::wstring msg) {
+    if (log_level_ <= ASSFONTS_INFO) {
+      Log(ASSFONTS_INFO, msg);
     }
   }
 
@@ -79,6 +99,16 @@ class Logger {
       Log(ASSFONTS_WARN, fmt, std::forward<T>(args)...);
     }
   }
+  void Warn(std::string msg) {
+    if (log_level_ <= ASSFONTS_WARN) {
+      Log(ASSFONTS_WARN, msg);
+    }
+  }
+  void Warn(std::wstring msg) {
+    if (log_level_ <= ASSFONTS_WARN) {
+      Log(ASSFONTS_WARN, msg);
+    }
+  }
 
   template <typename... T>
   void Error(fmt::format_string<T...> fmt, T&&... args) {
@@ -90,6 +120,16 @@ class Logger {
   void Error(fmt::wformat_string<T...> fmt, T&&... args) {
     if (log_level_ <= ASSFONTS_ERROR) {
       Log(ASSFONTS_ERROR, fmt, std::forward<T>(args)...);
+    }
+  }
+  void Error(std::string msg) {
+    if (log_level_ <= ASSFONTS_ERROR) {
+      Log(ASSFONTS_ERROR, msg);
+    }
+  }
+  void Error(std::wstring msg) {
+    if (log_level_ <= ASSFONTS_ERROR) {
+      Log(ASSFONTS_ERROR, msg);
     }
   }
 
@@ -119,6 +159,22 @@ class Logger {
     ReplaceNewline(msg);
 
     cb_(msg.c_str(), log_level);
+  }
+
+  void Log(ASSFONTS_LOG_LEVEL log_level, std::string msg) {
+    ReplaceNewline(msg);
+
+    cb_(msg.c_str(), log_level);
+  }
+
+  void Log(ASSFONTS_LOG_LEVEL log_level, std::wstring msg) {
+    std::string msg_u8;
+
+    msg_u8.append(WideToU8(msg));
+
+    ReplaceNewline(msg_u8);
+
+    cb_(msg_u8.c_str(), log_level);
   }
 
   void ReplaceNewline(std::string& str) {
