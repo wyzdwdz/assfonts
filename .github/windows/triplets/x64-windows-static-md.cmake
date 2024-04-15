367461ff892aee5214533433464cc8f4ac2978ec
wyzdwdz <wyzdwdz@gmail.com>
2024-04-16 00:19:07 +0200

    test

--- .github/windows/triplets/x64-windows-static-md.cmake
+++ .github/windows/triplets/x64-windows-static-md.cmake
@@ -2,4 +2,5 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchains/clang-windows.cmake")
+set(CMAKE_C_COMPILER "clang-cl.exe")
+set(CMAKE_CXX_COMPILER "clang-cl.exe")
