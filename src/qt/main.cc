1681c9ca947e5e8ac819874df1c04997e736c76e
wyzdwdz <wyzdwdz@gmail.com>
2024-05-10 12:22:20 +0200

    add qt wayland support

--- src/qt/main.cc
+++ src/qt/main.cc
@@ -26,6 +26,7 @@
 
 #ifdef __linux__
 Q_IMPORT_PLUGIN(QXcbIntegrationPlugin)
+Q_IMPORT_PLUGIN(QWaylandIntegrationPlugin)
 Q_IMPORT_PLUGIN(QIbusPlatformInputContextPlugin)
 Q_IMPORT_PLUGIN(QComposePlatformInputContextPlugin)
 Q_IMPORT_PLUGIN(QFcitx5PlatformInputContextPlugin)
